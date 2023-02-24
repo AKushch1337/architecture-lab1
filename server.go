a6923b7d8bcfb53a93330bc191f9e440db4c4feb
Artem Kushch <artemkusch2@gmail.com>
2023-02-24 20:19:34 +0200

    added getTime function

--- server.go
+++ server.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func getTime(w http.ResponseWriter, req *http.Request) {
+	currentTime := time.Now()
+	timeWithTimestamp := currentTime.Format(time.RFC3339)
+	data := map[string]string{
+		"time": timeWithTimestamp,
+	}
+	b, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if req.Method == "GET" {
+		fmt.Fprintf(w, string(b))
+	} else {
+		fmt.Fprintf(w, "Sorry, only GET request is supported.")
+	}
+}
